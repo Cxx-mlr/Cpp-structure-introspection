18301ffbfbcc4c1dbda6b316ad793c3a3c6d596d
Cxx <37257545+Cxx-mlr@users.noreply.github.com>
2019-12-11 16:34:28 -0500

    Update raw.cpp

--- include/raw.cpp
+++ include/raw.cpp
@@ -4,8 +4,10 @@
 #include <utility>
 #include <tuple>
 
-template <std::size_t>
-struct overload {};
+template <std::size_t> struct overload {};
+
+
+////
 
 template <class tstruct>
 auto as_tuple(tstruct&&, overload <0>) noexcept { return std::tuple{}; }
@@ -45,7 +47,8 @@ auto as_tuple(tstruct&& obj, overload <5>) noexcept {
 	return std::forward_as_tuple(_1, _2, _3, _4, _5);
 }
 
-//
+////
+
 
 // generic type
 template <std::size_t>
@@ -58,26 +61,30 @@ template <class tstruct, std::size_t... indexes>
 auto aggregate(std::index_sequence <indexes...>) -> decltype(std::decay_t <tstruct>{ auto_t <indexes>{}... }, void()) {
 }
 
-// aggregate initialization fails at n + 1 , n - 1 is the original size
+
+//
+
 template <class tstruct, std::size_t n = 0, class = std::void_t <>>
-struct rank : std::integral_constant <std::size_t, n - 1> {
+struct rank : std::integral_constant <std::size_t, n> {
 };
 
 template <class tstruct, std::size_t n>
-struct rank <tstruct, n, std::void_t <decltype(/**/ aggregate <tstruct>(std::make_index_sequence <n>()) /**/) >> : rank <tstruct, n + 1> {
+struct rank <tstruct, n, std::void_t <decltype(/**/ aggregate <tstruct>(std::make_index_sequence <n + 1>()) /**/) >> : rank <tstruct, n + 1> {
 };
-
-//
 
 template <class tstruct>
 inline constexpr std::size_t rank_v = rank <tstruct>::value;
 
 //
 
+
 template <std::size_t index, class tstruct>
 decltype(auto) get(tstruct&& obj) {
 	return std::get <index>(as_tuple(std::forward <tstruct>(obj), overload <rank_v <tstruct>>{}));
 }
+
+
+//
 
 template <class tstruct, class ptr_f, std::size_t... indexes>
 void for_each_impl(tstruct&& obj, ptr_f f, std::index_sequence <indexes...>) {
@@ -89,6 +96,11 @@ void for_each(tstruct&& obj, ptr_f f) {
 	for_each_impl(std::forward<tstruct>(obj), f, std::make_index_sequence <rank_v <tstruct>>());
 }
 
+//
+
+
+//
+
 template <class ptr_f, class tstruct, std::size_t... indexes>
 void apply_impl(ptr_f f, tstruct&& obj, std::index_sequence <indexes...>) {
 	f(get <indexes>(std::forward <tstruct>(obj))...);
@@ -98,3 +110,5 @@ template <class ptr_f, class tstruct>
 void apply(ptr_f f, tstruct&& obj) {
 	apply_impl(f, std::forward <tstruct>(obj), std::make_index_sequence <rank_v <tstruct>>());
 }
+
+//
